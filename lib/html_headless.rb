1e1b9286994e0b07fed75784aa9a086ab8a0801c
C Viktor <vchekryzhov@ya.ru>
2019-12-11 14:29:07 +0300

    merge

--- lib/html_headless.rb
+++ lib/html_headless.rb
@@ -1,37 +1,38 @@
-class HtmlHeadless
-  def self.callback_method
-    :share_image_generate
-  end
+module HtmlHeadless
+  require 'fileutils'
+
   def initialize(template)
     @template = File.read(Rails.root.join('app/views').join(template+".html.erb"))
   end
 
+  included do
+    after_commit :share_image_generate
+  end
+
   def to_image(obj, uploader_method, width=1200, height=630)
     html = ERB.new(@template.html_safe).result(binding)
-    html_file = Tempfile.new(['share','.html'])
     screenshot_file = Tempfile.new(['screen','.png'])
+    File.open(html_file_path, 'w') {|f| f.write(html) }
+    screenshot_file = Tempfile.new(['screen','.jpg'])
     begin
-      html_file.write(html)
-      html_file.rewind
       cmd = "'#{chrome}'
         --headless
         --screenshot=#{screenshot_file.path}
         --window-size=#{width},#{height}
         --disable-gpu
-        --disable-features=NetworkService #{html_file.path}".gsub("\n",' ')
+        --disable-features=NetworkService #{html_file_path}".gsub("\n",' ')
       `#{cmd}`
       if $?.success?
-        obj.class.skip_callback(:commit, :after, HtmlHeadless.callback_method)
+        obj.class.skip_callback(:commit, :after, :share_image_generate)
         obj.send("#{uploader_method}=", screenshot_file)
         obj.save
-        obj.class.set_callback(:commit, :after, HtmlHeadless.callback_method)
+        obj.class.set_callback(:commit, :after, :share_image_generate)
       else
         raise "result = #{$?}; command = #{cmd}"
       end
     ensure
-       html_file.close
+       FileUtils.rm(html_file_path)
        screenshot_file.close
-       html_file.unlink
        screenshot_file.unlink
     end
   end
@@ -44,5 +45,13 @@ class HtmlHeadless
     else
       raise StandardError.new "host os don't detected"
     end
+  end
+
+  def html_file_path
+    @path = Rails.public_path.join(SecureRandom.urlsafe_base64.downcase + ".html")
+  end
+
+  def sharing_image_generate
+
   end
 end
